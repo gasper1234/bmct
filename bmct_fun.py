3c39e17d1f18ebb7ea9acec18e8ed4b1cf3fc6c1
gasper1234 <gasperjalen@gmail.com>
2023-04-18 18:12:19 +0200

    lj 18.4

--- bmct_fun.py
+++ bmct_fun.py
@@ -3,21 +3,23 @@ import numpy as np
 import time
 from numba import njit
 
-@njit
-def random_more(val):
+# monte carlo del simulacije, za pospesitev kode je uporabljena numba
+
+@njit # poisce nakljucno vrednost med 0 in 1, ki je manjsa od val
+def random_less(val):
 	r_val = np.random.random()
 	while r_val < val:
 		r_val = np.random.random()
 	return r_val
 
-@njit
-def random_less(val):
+@njit # poisce nakljucno vrednost med 0 in 1, ki je vecja od val
+def random_more(val):
 	r_val = np.random.random()
 	while r_val > val:
 		r_val = np.random.random()
 	return r_val
 
-@njit
+@njit # poisce pozicijo v vektorju p, ki pripada nakljucenmu stevilu
 def find_pos(vec, val):
 	acum = vec[0]
 	for i in range(len(vec)):
@@ -26,29 +28,36 @@ def find_pos(vec, val):
 		else:
 			acum += vec[i+1]
 
-@njit
+@njit # 1 korak Monte Carlo
 def step(vec, diff):
 	vec_1 = np.copy(vec)
-	m1, m2 = random_more(vec[0]), random_more(vec[0])
-	p1, p2 = random_less(1-vec[-1]), random_less(1-vec[-1])
+	# izbira pozicij v vetkorju, ki jima zmanjsamo stevilo stranic
+	m1, m2 = random_less(vec[0]), random_less(vec[0])
 	m_pos_1, m_pos_2 = find_pos(vec, m1), find_pos(vec, m2)
+	# izbira pozicij v vetkorju, ki jima povecamo stevilo stranic
+	p1, p2 = random_more(1-vec[-1]), random_more(1-vec[-1])
 	p_pos_1, p_pos_2 = find_pos(vec, p1), find_pos(vec, p2)
+	# zmanjsamo stevilo stranic (prestavimo celico iz p_m v p_m-1)
 	vec_1[int(m_pos_1)] -= diff
 	vec_1[int(m_pos_2)] -= diff
 	vec_1[int(m_pos_1-1)] += diff
 	vec_1[int(m_pos_2-1)] += diff
+	# povecamo stevilo stranic (prestavimo celico iz p_m v p_m+1)
 	vec_1[int(p_pos_1)] -= diff
 	vec_1[int(p_pos_2)] -= diff
 	vec_1[int(p_pos_1+1)] += diff
 	vec_1[int(p_pos_2+1)] += diff
 	return vec_1
 
-@njit
+@njit # iteracija korakov
 def walk(t_s, diff):
 	for i in range(1, len(t_s)):
 		t_s[i] = step(t_s[i-1], diff)
 	return t_s
 
+# markovske verige
+
+# matrika T
 def make_T(dim, dt):
 	T_1 = np.diag(np.ones((dim))-dt)
 	T_2 = np.diag(np.ones((dim-1))*dt/2, 1) + np.diag(np.ones((dim-1))*dt/2, -1)
@@ -57,13 +66,73 @@ def make_T(dim, dt):
 	T[dim-1, dim-1] = 1-dt/2
 	return T
 
-print(make_T(10, 0.1))
-
+# casovni razvoj z matriko T
 def propag(t_s, diff):
 	T = make_T(len(t_s[0]), diff)
 	check = len(t_s)//100
 	for i in range(1, len(t_s)):
 		t_s[i] = T.dot(t_s[i-1])
+		if i % check == 0:
+			print('norm', np.sum(t_s[i]))
+	return t_s
+
+# od tu naprej le se preizkusanje kako se obnasajo drugacne matrike T
+
+def make_T_up(dim, dt):
+	T_1 = np.diag(np.ones((dim))-dt)
+	T_2 = np.diag(np.ones((dim-1))*dt, -1)
+	T = T_1 + T_2
+	T[dim-1, dim-1] = 1
+	return T
+
+def make_T_down(dim, dt):
+	T_1 = np.diag(np.ones((dim))-dt)
+	T_2 = np.diag(np.ones((dim-1))*dt, 1)
+	T = T_1 + T_2
+	T[0, 0] = 1
+	return T
+
+
+def propag_12(t_s, diff):
+	T_1 = make_T_up(len(t_s[0]), diff)
+	T_2 = make_T_down(len(t_s[0]), diff)
+	check = len(t_s)//100
+	for i in range(1, len(t_s)):
+		if i % 2 == 0:
+			t_s[i] = T_2.dot(t_s[i-1])
+		else:
+			t_s[i] = T_1.dot(t_s[i-1])
+		if i % check == 0:
+			print('norm', np.sum(t_s[i]))
+	return t_s
+
+def make_T_sides(dim, dt):
+	sides = np.arange(3, 3+dim, 1)
+	T_1 = np.diag(np.ones((dim))-sides*dt)
+	T_2 = np.diag(np.ones((dim-1))*sides[1:]*dt/2, 1) + np.diag(np.ones((dim-1))*sides[:-1]*dt/2, -1)
+	T = T_1 + T_2
+	T[0, 0] = 1-T[1, 0]
+	T[dim-1, dim-1] = 1-T[dim-2, dim-1]
+	return T
+
+def make_T_leak(dim, dt):
+	T_1 = np.diag(np.ones((dim))-dt)
+	T_2 = np.diag(np.ones((dim-1))*dt/2, 1) + np.diag(np.ones((dim-1))*dt/2, -1)
+	T = T_1 + T_2
+	T[0, 0] = 1-dt/2
+	T[1, 0] = 3*dt-4
+	T[dim-1, dim-1] = 1-dt/2
+	return T
+
+def propag1(t_s, diff):
+	dim = len(t_s[0])
+	sides = np.arange(3, 3+dim, 1)
+	T = make_T(dim, diff)
+	check = len(t_s)//100
+	for i in range(1, len(t_s)):
+		t_s[i] = T.dot(t_s[i-1])
+		t_s[i, 0] += diff/dim
+		t_s[i] /= np.sum(t_s[i])
 		if i % check == 0:
 			print('norm', np.sum(t_s[i]))
 	return t_s
--- mc_simulation.py
+++ mc_simulation.py
@@ -1,36 +1,61 @@
 from bmct_fun import *
 
-dim = 10
-N = 10000
+# število obravnavanih poligonov (pri 7 dobimo enakomerno porazdelitev tudi za monte carlo)
+# pri 20 ali vec se porazdelitev za monte carlo ne spreminja vec bistveno
+dim = 20
+# število celic
+N = 100000
 diff = 1/N
-t = 100000
+# čas simulacije (število)
+t = 10000000
 t_s = np.zeros((t, dim))
-plot_numbers = [0, 2, 3, 4]
-cols = ['k', 'red', 'blue', 'orange', 'green', 'yellow']
 
-
+# zacetni pogoj (mreza sestkotnika)
 init_vec = np.zeros((dim))
 init_vec[3] = 1
 
 t_s[0] = init_vec
 
-# izracun propagacija
+# izracun markovske verige
+start= time.time()
 t_s_prop = propag(np.copy(t_s), diff*4)
+print(time.time()-start)
 
-# izracun MC
 
+# izracun monte carlo
 start= time.time()
 t_s_mc = walk(t_s, diff)
 print(time.time()-start)
 
 
-# plot
+t_s = np.linspace(0, t, t/100)
+# plot casovnega poteka
+plot_numbers = [0, 1, 2, 3] # razvoj katerih N-kotnikov prikazemo
+cols = ['k', 'red', 'blue', 'orange', 'green', 'yellow']
 for i in range(len(plot_numbers)):
-	plt.plot(t_s_prop[:, plot_numbers[i]], '--', color=cols[i], label=plot_numbers[i]+3)
-	plt.plot(t_s_mc[:, plot_numbers[i]], color=cols[i], alpha=0.5)
+	plt.plot(t_s, t_s_prop[:, plot_numbers[i]][::100], '--', color=cols[i], label=plot_numbers[i]+3)
+	plt.plot(t_s, t_s_mc[:, plot_numbers[i]][::100], color=cols[i], alpha=0.5)
 plt.xscale('log')
 plt.yscale('log')
-plt.ylim(0.01, 1)
+plt.ylim(0.005, 1.05)
 plt.xlim(10, t)
 plt.legend()
+plt.show()
+
+
+# izracun naklona koncne porazdelitve pri monte carlo, ta je kot kazejo rezultati eksponentna
+x_s = np.arange(1, dim+1, 1)
+y_s = np.log(t_s_mc[-1])
+m,b = np.polyfit(x_s, y_s, 1)
+print('naklon', m, 'zacetna vrednost', b)
+
+# plot zacetne in koncne porazdelitve
+fig, ax = plt.subplots(2)
+x_s = np.arange(1, dim+1, 1)
+ax[0].bar(x_s, t_s_mc[-1],label='Monte Carlo stacionarno') # plot koncne porazdelitve
+ax[0].plot(x_s, np.exp(b)*np.exp(m*x_s), 'rx') # plot fita
+ax[0].legend()
+ax[1].bar(x_s, t_s_prop[-1], label='Markov chain stacinarno') # plot zacente
+ax[1].legend()
+ax[0].set_yscale('log')
 plt.show()
